9eb0e6ba52b3b356fb44e81809617ac561bf7f2c
rei905 <dexter.zurbruegg@tie-international.com>
2024-10-09 10:34:13 +0200

    controller using repository methods

--- src/main/java/com/tie_international/multiuserapp/controllers/AdminController.java
+++ src/main/java/com/tie_international/multiuserapp/controllers/AdminController.java
@@ -1,19 +1,27 @@
 package com.tie_international.multiuserapp.controllers;
 
-import org.springframework.stereotype.Controller;
+import com.tie_international.multiuserapp.entities.Person;
+import com.tie_international.multiuserapp.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api")
+@RequestMapping("api/")
 public class AdminController {
-    /*
-    @GetMapping("/admin/getPersonById/{id}")
-    public getPersonById(@PathVariable("id")String id)  {
-        return ""
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @GetMapping("admin/getPersonById/{id}")
+    public Person getPersonById(@PathVariable("id") Long id) {
+        return personRepository.getPersonById(id);
     }
 
-     */
+    @GetMapping("admin/getPersonByName/{name}")
+    public Person getPersonByLastName(@PathVariable("name") String name) {
+        return personRepository.getPersonByLastName(name);
+    }
 }
--- src/main/java/com/tie_international/multiuserapp/repositories/PersonRepository.java
+++ src/main/java/com/tie_international/multiuserapp/repositories/PersonRepository.java
@@ -2,10 +2,15 @@ package com.tie_international.multiuserapp.repositories;
 
 import com.tie_international.multiuserapp.entities.Person;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface PersonRepository extends CrudRepository<Person, Long> {
     Person getPersonByLastName(String lastName);
     Person getPersonById(Long id);
+
 }
